e346d1415f31e676e103163efb1e4738a78c6c95
Yizhou Zhang <essiazhang@gmail.com>
2022-10-18 21:11:52 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,9 @@ const routes = require('./routes');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+
+const { ValidationError } = require('sequelize');
+
 const app = express();
 
 // connect morgan middleware for logging info about requests and responses
@@ -46,5 +49,37 @@ app.use(
 
 // connect all routes
 app.use(routes);
+
+// connect error handler
+    // Resource Not Found Handler
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+    // Sequelize Error Handler
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+
+    next(err);
+});
+
+    // Format Error and Return JSON Error Handler
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
