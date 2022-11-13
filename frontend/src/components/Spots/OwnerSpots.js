cde0030958c6302147b8fbbe437af3214a837d83
Yizhou Zhang <essiazhang@gmail.com>
2022-11-13 13:39:05 -0800

    changed current spots layout and update button.

--- frontend/src/components/Spots/OwnerSpots.js
+++ frontend/src/components/Spots/OwnerSpots.js
@@ -22,34 +22,80 @@ export default function OwnerSpots({ isLoaded }) {
   if (ownerSpots) ownerSpotsArr = Object.values(ownerSpots);
 
   return (
-    <div className='all-spots-wrapper'>
-      <div className='all-spots-sub-wrapper'>
-        {
-          ownerSpotsArr?.map(spot => {
-            const { previewImage, city, state, avgRating, price, id } = spot;
-            return (
-              <div key={id} className='owner-spots-wrapper'>
-                <NavLink to={`/spots/${id}`} className='link-wrapper'>
-                  <div className='individual-spot-wrapper'>
-                    <div className='image-div'>
-                      <img src={`${previewImage}`} alt='preview' />
-                    </div>
-                    <div className='short-info-wrapper'>
-                      <span className='cityState'>{city}, {state}</span>
-                      <span style={{ color: '#717171' }}><b>${price}</b> night</span>
-                      <span className='rating-wrapper'>
-                        <i className="fa-solid fa-star" />
-                        {avgRating ? <span>{avgRating.toFixed(1)}</span> : null}
-                      </span>
-                    </div>
-                  </div>
-                </NavLink>
-                <div className='modify-button-div'><button className='modify-buttons'>Update</button></div>
-                <div className='modify-button-div'><button className='modify-buttons'>Delete</button></div>
-              </div>
-            )
-          })
-        }
+    <div className='owner-spots-wrapper'>
+      <div className='owner-spots-sub-wrapper'>
+        <h4>
+          {ownerSpotsArr.length > 0 ? `${ownerSpotsArr.length} listings` : 'No listing'}
+        </h4>
+        <table className='owner-spots-table'>
+          <thead>
+            <tr className='owner-spots-header-row' >
+              <th><span>LISTING</span></th>
+              <th><span></span></th>
+              <th><span>LOCATION</span></th>
+              <th><span>LAST MODIFIED</span></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              ownerSpotsArr?.map(spot => {
+                const { previewImage, city, state, name, id, updatedAt } = spot;
+                const date = new Date(updatedAt);
+                const month = date.toLocaleString('en-US', { month: 'long' });
+                const day = date.getDate();
+
+                return (
+                  <tr key={id}>
+                    <td>
+                      <div className="table-image-wrapper" style={{display:'flex'}}>
+                        <div className='table-image-div' >
+                          <img src={`${previewImage}`} alt='preview' />
+                        </div>
+                        <span><b>{name}</b></span>
+                      </div>
+                    </td>
+                    <td>
+                      <div className='modify-button-div'>
+                        <button className='modify-buttons first'>Update</button>
+                        <button className='modify-buttons'>Delete</button>
+                      </div>
+                    </td>
+                    <td>
+                      <span>{city}, {state}</span>
+                    </td>
+                    <td>
+                      <span>{month} {day}</span>
+                    </td>                    
+                  </tr>
+                )
+              })
+            }
+            {/* ownerSpotsArr?.map(spot => {
+              
+              // return (
+              //   <div key={id} className='owner-spots-wrapper'>
+              //     <NavLink to={`/spots/${id}`} className='link-wrapper'>
+              //       <div className='individual-spot-wrapper'>
+              //         <div className='image-div'>
+              //           <img src={`${previewImage}`} alt='preview' />
+              //         </div>
+              //         <div className='short-info-wrapper'>
+              //           <span className='cityState'>{city}, {state}</span>
+              //           <span style={{ color: '#717171' }}><b>${price}</b> night</span>
+              //           <span className='rating-wrapper'>
+              //             <i className="fa-solid fa-star" />
+              //             {avgRating ? <span>{avgRating.toFixed(1)}</span> : null}
+              //           </span>
+              //         </div>
+              //       </div>
+              //     </NavLink>
+              //     <div className='modify-button-div'><button className='modify-buttons'>Update</button></div>
+              //     <div className='modify-button-div'><button className='modify-buttons'>Delete</button></div>
+              //   </div>
+              // )
+            }) */}
+          </tbody>
+        </table>
       </div>
     </div>
   )
