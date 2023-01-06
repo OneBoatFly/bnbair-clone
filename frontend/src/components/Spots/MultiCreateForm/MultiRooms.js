0fa8b9fde6b4e3af92948c1e68a4490e1adafaaf
Yizhou Zhang <essiazhang@gmail.com>
2023-01-05 22:09:23 -0800

    css to rooms page.

--- frontend/src/components/Spots/MultiCreateForm/MultiRooms.js
+++ frontend/src/components/Spots/MultiCreateForm/MultiRooms.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './MultiRooms.css';
 
 export default function MultiRooms({ formData, setFormData }) {
 
@@ -8,13 +9,13 @@ export default function MultiRooms({ formData, setFormData }) {
       <div className='multi-create-rooms-single-div'>
         <span>Guests</span>
         <div className='multi-create-rooms-toggle-div'>
-          <i className={`fa-solid fa-minus ${formData.guests === 1 && 'toggle-disabled'}`} 
+          <i className={`fa-solid fa-minus multi-create-toggle-icon ${formData.guests === 1 && 'toggle-disabled'}`} 
             onClick={() => {
               if (formData.guests <= 1) return;
               setFormData({ ...formData, guests: formData.guests - 1 })
           }}/>
-          <span>{formData.guests}</span>
-          <i className={`fa-solid fa-plus ${formData.guests === 16 && 'toggle-disabled'}`} 
+          <span className="multi-create-rooms-number">{formData.guests}</span>
+          <i className={`fa-solid fa-plus multi-create-toggle-icon ${formData.guests === 16 && 'toggle-disabled'}`} 
             onClick={() => {
               if (formData.guests >= 16) return;
               setFormData({ ...formData, guests: formData.guests + 1 })
@@ -25,13 +26,13 @@ export default function MultiRooms({ formData, setFormData }) {
       <div className='multi-create-rooms-single-div'>
         <span>Bedrooms</span>
         <div className='multi-create-rooms-toggle-div'>
-          <i className={`fa-solid fa-minus ${formData.bedrooms === 0 && 'toggle-disabled'}`}
+          <i className={`fa-solid fa-minus multi-create-toggle-icon ${formData.bedrooms === 0 && 'toggle-disabled'}`}
             onClick={() => {
               if (formData.bedrooms <= 0) return;
               setFormData({ ...formData, bedrooms: formData.bedrooms - 1 })
             }} />
-          <span>{formData.bedrooms}</span>
-          <i className={`fa-solid fa-plus ${formData.bedrooms === 50 && 'toggle-disabled'}`}
+          <span className="multi-create-rooms-number">{formData.bedrooms}</span>
+          <i className={`fa-solid fa-plus multi-create-toggle-icon ${formData.bedrooms === 50 && 'toggle-disabled'}`}
             onClick={() => {
               if (formData.bedrooms >= 50) return;
               setFormData({ ...formData, bedrooms: formData.bedrooms + 1 })
@@ -42,13 +43,13 @@ export default function MultiRooms({ formData, setFormData }) {
       <div className='multi-create-rooms-single-div'>
         <span>Beds</span>
         <div className='multi-create-rooms-toggle-div'>
-          <i className={`fa-solid fa-minus ${formData.beds === 1 && 'toggle-disabled'}`}
+          <i className={`fa-solid fa-minus multi-create-toggle-icon ${formData.beds === 1 && 'toggle-disabled'}`}
             onClick={() => {
               if (formData.beds <= 1) return;
               setFormData({ ...formData, beds: formData.beds - 1 })
             }} />
-          <span>{formData.beds}</span>
-          <i className={`fa-solid fa-plus ${formData.beds === 16 && 'toggle-disabled'}`}
+          <span className="multi-create-rooms-number">{formData.beds}</span>
+          <i className={`fa-solid fa-plus multi-create-toggle-icon ${formData.beds === 16 && 'toggle-disabled'}`}
             onClick={() => {
               if (formData.beds >= 16) return;
               setFormData({ ...formData, beds: formData.beds + 1 })
@@ -59,13 +60,13 @@ export default function MultiRooms({ formData, setFormData }) {
       <div className='multi-create-rooms-single-div'>
         <span>Bathrooms</span>
         <div className='multi-create-rooms-toggle-div'>
-          <i className={`fa-solid fa-minus ${formData.bathrooms === 0.5 && 'toggle-disabled'}`}
+          <i className={`fa-solid fa-minus multi-create-toggle-icon ${formData.bathrooms === 0.5 && 'toggle-disabled'}`}
             onClick={() => {
               if (formData.bathrooms <= 0.5) return;
               setFormData({ ...formData, bathrooms: formData.bathrooms - 0.5 })
             }} />
-          <span>{formData.bathrooms}</span>
-          <i className={`fa-solid fa-plus ${formData.bathrooms === 50 && 'toggle-disabled'}`}
+          <span className="multi-create-rooms-number">{formData.bathrooms}</span>
+          <i className={`fa-solid fa-plus multi-create-toggle-icon ${formData.bathrooms === 50 && 'toggle-disabled'}`}
             onClick={() => {
               if (formData.bathrooms >= 50) return;
               setFormData({ ...formData, bathrooms: formData.bathrooms + 0.5 })
