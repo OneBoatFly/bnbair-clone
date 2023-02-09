d46265c5fb31684970277f404429be4bef3807fb
Yizhou Zhang <essiazhang@gmail.com>
2023-02-08 20:26:18 -0800

    add auto generate description

--- backend/routes/api/spotai.js
+++ backend/routes/api/spotai.js
@@ -7,6 +7,7 @@ const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
+    // apiKey: 'abc'
 });
 
 const openai = new OpenAIApi(configuration);
--- frontend/src/components/Spots/MultiCreateForm/MultiTitle.js
+++ frontend/src/components/Spots/MultiCreateForm/MultiTitle.js
@@ -4,10 +4,12 @@ import { csrfFetch } from '../../../store/csrf';
 
 export default function MultiTitle({ formData, setFormData, hasSubmitted, titleErrors, setTitleErrors }) {
   const [AILoading, setAILoading] = useState(false);
+  const [AIError, setAIError] = useState('');
 
   const AIClick = async () => {
-    console.log('AI Button')
+    // console.log('AI Button')
     setAILoading(true)
+    setAIError('')
 
     const query = {
       city: formData.city,
@@ -20,26 +22,25 @@ export default function MultiTitle({ formData, setFormData, hasSubmitted, titleE
       amenitySafety: formData.amenitySafety,
       type: 'title'
     }
-    console.log(query)
+    // console.log(query)
 
     const options = {
       method: 'POST',
       body: JSON.stringify(query)
     };
 
-    const response = await csrfFetch('/api/spotai', options);
-
-    if (response.ok) {
-      let data = await response.json()
-      console.log(data) // "\n\nCozy Woodinville Home: 1BR, WiFi, Kitchen+"
-      console.log(data.result)
-      setFormData({ ...formData, name: data.result.slice(2) })
-    } else{
-      console.log('error')
-      console.log(response)
-    }
-
-    setAILoading(false)
+    csrfFetch('/api/spotai', options)
+      .then(async (response) => {
+        // console.log(response)
+        let data = await response.json()
+        setFormData({ ...formData, name: data.result.slice(2) })
+        setAILoading(false)
+      })
+      .catch((error) => {
+        // console.log('error', error)
+        setAIError('An Error occurred. Please try again.')
+        setAILoading(false)
+      })
   }
   
   useEffect(() => {
@@ -59,9 +60,17 @@ export default function MultiTitle({ formData, setFormData, hasSubmitted, titleE
   return (
     <div className='multi-create-title'>
       <div className='create-spot'>
-        <div>
+        <div className='ai-button-wrapper'>
           <button className='main-create-button ai-help' onClick={AIClick}>Auto-Generate a Title</button>
-          {AILoading && <span>Loading...</span>}
+          {AILoading && 
+            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+          }
+          {AIError.length > 0 && 
+            <div className='error-messages-wrapper'>
+              <i className="fa-sharp fa-solid fa-circle-exclamation"></i>
+              <span className='error-messages'>{AIError}</span>
+            </div>
+          }
         </div>
         <div className='outline-wrapper name-wrapper multi-create-title-spot-name'>
           <div className='create-spot-name'>
